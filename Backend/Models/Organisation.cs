d0c85fab5d5d5905951652675b92353a242130a4
My <myglimsjo@gmail.com>
2026-03-25 14:57:09 +0100

    Added more information to User.cs and created Organisation.cs

--- Backend/Models/Organisation.cs
+++ Backend/Models/Organisation.cs
@@ -1,17 +1,14 @@
 namespace Backend.Models;
 
-public class User
+public class Organisation
 {
     public int Id { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public DateTime DateOfBirth { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string OrgNumber { get; set; } = string.Empty;
+    public string TaxNumber { get; set; } = string.Empty;
     public string Address { get; set; } = string.Empty;
     public string PostalCode { get; set; } = string.Empty;
     public string City { get; set; } = string.Empty;
     public string Country { get; set; } = string.Empty;
     public string PhoneNumber { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = null!;
-    public int OrganisationId { get; set; }
 }
--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14d4dafabf0aa3212aeaa2a2ab3d628efd163b83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a27807e4af63349508c85992de1b1e00e046c6c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
