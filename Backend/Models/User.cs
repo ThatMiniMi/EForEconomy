a27807e4af63349508c85992de1b1e00e046c6c7
My <myglimsjo@gmail.com>
2026-03-25 14:45:43 +0100

    Cleaned up program.cs, created simple folder structure and user.cs

---
+++ Backend/Models/User.cs
@@ -0,0 +1,11 @@
+namespace Backend.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = null!;
+}
