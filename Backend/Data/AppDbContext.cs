e0d35813fd853b9fdcfb1d0b198e742b5356b0ac
My <myglimsjo@gmail.com>
2026-03-25 15:38:34 +0100

    Created AppDbContext.cs

---
+++ Backend/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Backend.Models;
+
+namespace Backend.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<EmployeeContract> EmployeeContracts { get; set; } = null!;
+    public DbSet<Invoice> Invoices { get; set; } = null!;
+    public DbSet<Organisation> Organisations { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<WorkSession> WorkSessions { get; set; } = null!;
+}
